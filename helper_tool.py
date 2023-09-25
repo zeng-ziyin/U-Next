041e9fa53e8a81edb26c692e229ed3b5f8d92191
zeng-ziyin <66163981+zeng-ziyin@users.noreply.github.com>
2023-09-25 20:46:27 +0800

    Update helper_tool.py

--- helper_tool.py
+++ helper_tool.py
@@ -42,31 +42,6 @@ class ConfigSemanticKITTI:
     saving_path = None
 
 
-# class ConfigSemanticKITTI:
-#     k_n = 16  # KNN
-#     num_layers = 5  # Number of layers
-#     num_points = 65536  # Number of input points
-#     num_classes = 19  # Number of valid classes
-#     sub_grid_size = 0.04  # preprocess_parameter
-#
-#     batch_size = 5  # batch_size during training
-#     val_batch_size = 20  # batch_size during validation and test
-#     train_steps = 750  # Number of steps per epochs
-#     val_steps = 500  # Number of validation steps per epoch
-#
-#     sub_sampling_ratio = [4, 4, 4, 4, 2]  # sampling ratio of random sampling at each layer
-#     d_out = [16, 64, 128, 256, 512]  # feature dimension
-#
-#     noise_init = 0.0  # noise initial parameter
-#     max_epoch = 100  # maximum epoch during training
-#     learning_rate = 1e-2  # initial learning rate
-#     lr_decays = {i: 0.95 for i in range(0, 500)}  # decay rate of learning rate
-#
-#     train_sum_dir = 'train_log'
-#     saving = True
-#     saving_path = None
-
-
 class ConfigSensatUrban:
     k_n = 16  # KNN
     num_layers = 5  # Number of layers
@@ -78,31 +53,6 @@ class ConfigSensatUrban:
     val_batch_size = 20  # batch_size during validation and test
     train_steps = 750  # Number of steps per epochs
     val_steps = 200  # Number of validation steps per epoch
-
-    sub_sampling_ratio = [4, 4, 4, 4, 2]  # sampling ratio of random sampling at each layer
-    d_out = [16, 64, 128, 256, 512]  # feature dimension
-
-    noise_init = 0.0  # noise initial parameter
-    max_epoch = 100  # maximum epoch during training
-    learning_rate = 1e-2  # initial learning rate
-    lr_decays = {i: 0.95 for i in range(0, 500)}  # decay rate of learning rate
-
-    train_sum_dir = 'train_log'
-    saving = True
-    saving_path = None
-
-
-class ConfigSensatUrban_ignore:
-    k_n = 16  # KNN
-    num_layers = 5  # Number of layers
-    num_points = 65536  # Number of input points
-    num_classes = 7  # Number of valid classes
-    sub_grid_size = 0.2  # preprocess_parameter
-
-    batch_size = 2  # batch_size during training
-    val_batch_size = 10  # batch_size during validation and test
-    train_steps = 1000  # Number of steps per epochs
-    val_steps = 300  # Number of validation steps per epoch
 
     sub_sampling_ratio = [4, 4, 4, 4, 2]  # sampling ratio of random sampling at each layer
     d_out = [16, 64, 128, 256, 512]  # feature dimension
